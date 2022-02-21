406df081038ab0b2c6adb6d4181f9b728213e54f
chaehoon <youspend@gmail.com>
2022-02-21 16:54:08 +0900

    initial commit

---
+++ src/main/java/com/foorun/unieat/pigeon/UnieatPigeonApplication.java
@@ -0,0 +1,13 @@
+package com.foorun.unieat.pigeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnieatPigeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnieatPigeonApplication.class, args);
+    }
+
+}
