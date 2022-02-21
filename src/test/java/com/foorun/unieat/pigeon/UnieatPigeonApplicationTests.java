e5a3062c987d656a53e75d9b6c59bd62f6c3d539
chaehoon <youspend@gmail.com>
2022-02-21 17:02:12 +0900

    remove : MailProperties Class

--- src/test/java/com/foorun/unieat/pigeon/UnieatPigeonApplicationTests.java
+++ src/test/java/com/foorun/unieat/pigeon/UnieatPigeonApplicationTests.java
@@ -1,21 +1,13 @@
 package com.foorun.unieat.pigeon;
 
-import com.foorun.unieat.pigeon.config.properties.mail.MailProperties;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.net.PasswordAuthentication;
 
 @SpringBootTest
 class UnieatPigeonApplicationTests {
-    @Autowired
-    private MailProperties mailProperties;
 
     @Test
     void contextLoads() {
-        System.out.println("mailProperties --> " + mailProperties);
-
     }
 
 }
